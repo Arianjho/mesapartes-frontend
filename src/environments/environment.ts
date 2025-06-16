11153d74cc4b53b0f718ef086d8d7bd5eddc2a58
Arianjho <arianjosafat67@gmail.com>
2025-06-16 08:47:01 -0500

    feat: Configuracion de enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  api_url: 'http://localhost:8085/api'
+    production: false,
+    api_url: 'https://mesadmin.arianjosafat.com/api'
 };
 
 /*
